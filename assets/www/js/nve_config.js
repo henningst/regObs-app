26b40ca7d84cad73b0927c76c157dc551e8fb374
Philipp Beau <PhilippB3au@gmail.com>
2012-05-24 11:17:16 +0200

    localisation

--- assets/www/js/nve_config.js
+++ assets/www/js/nve_config.js
@@ -42,13 +42,3 @@ WATER_GEO_HAZARD = 60;
  */
 OMRAADE_ID_OFFSET = 100;
 
-/*
- * Text
- */
-LOGGED_IN  = "Logget inn !";
-ERROR_LOGIN =  "No internet ?!";
-LOGGING_IN = "Logger inn... ";
-PROCESS_PICTURE = "Prosess bilde... ";
-UPLOADING = "Laster opp... ";
-OK = "Ok";
-SEND_EMAIL = "Send epost";
---
+++ assets/www/js/string.js
@@ -0,0 +1,18 @@
+/*
+ * Text
+ */
+LOGGED_IN  = "Logget inn !";
+
+ERROR_LOGIN =  "No internet ?!";
+
+LOGGING_IN = "Logger inn... ";
+
+PROCESS_PICTURE = "Prosess bilde... ";
+
+UPLOADING = "Laster opp... ";
+
+OK = "Ok";
+
+SEND_EMAIL = "Send epost";
+
+HENDELSE = "Ulykke / Hendelse"
