3a56d2311e004632286cc6b74ad92d6ba1597fd5
ragnarekker <ragnar.ekker@gmail.com>
2012-05-31 15:15:58 +0300

    added ABOUT_REGOBS

--- assets/www/js/string.js
+++ assets/www/js/string.js
@@ -57,3 +57,4 @@ DIRT_TEXT =
 
 SNOW_PICTURE ="<H3>Hvordan ta et godt bilde</H3><ul><li>Tenk på at et bilde skal beskrive noe som kan nyttes av andre når de skal gjøre seg en vurdering av forholdene.</li><li>Hvis bildet skal sees i sammenheng med en annen observasjon, velg den i nedtrekksmenyen.</li><li>Svært nyttige bilder å få inn er: snøfordelig i området, skred, skavler, snøprofilet ift terrenget rundt, fete skibilder.</li><li>Ikke så nyttige bilder å få inn: noen som graver et snøprofil.</li></ul>";
 
+ABOUT_REGOBS = "Mer text her";
