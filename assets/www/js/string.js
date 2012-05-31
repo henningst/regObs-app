a5b737f99a501e1701663d4e5e80e5702fbfa3b7
ragnarekker <ragnar.ekker@gmail.com>
2012-05-31 22:33:06 +0300

    Infotekst for snow complete. Waiting with the rest before syntax has been discussed.

--- assets/www/js/string.js
+++ assets/www/js/string.js
@@ -55,6 +55,10 @@ DIRT_TEXT =
 		"Gjelder for fylket/ varslingsregionen.",
 	 ];
 
+
+/*
+ * Hjelpetekst til observasjonene
+ */
 SNOW_PICTURE =  
 	"<H3>Hvordan ta et godt bilde</H3>"
 	+ "<ul>" 
@@ -63,19 +67,30 @@ SNOW_PICTURE =
 	+ "<li>Svært nyttige bilder å få inn er: snøfordelig i området, skred, skavler, snøprofilet ift terrenget rundt, fete skibilder.</li>"
 	+ "<li>Ikke så nyttige bilder å få inn: noen som graver et snøprofil.</li>"
 	+ "</ul>";
-              
+
 SNOW_DANGERSIGN =
-	"</h3>Hva er det med disse faretegnene egentlig?</h3>"
-	+ "<p>Faretegn kan man bruke til å vurdere hvor stabil snøen er.</p>"
+	" </h3>Hva er det med disse faretegnene egentlig?</h3>"
+	+ "<p>Faretegn kan man bruke til &aring; vurdere hvor stabil sn&oslash;en er.</p>"
 	+ "<ul>"
-	+ "<li>Hvis det drønner eller går naturlige skred vet vi at vi trolig er på en faregrad 3-Markert.</li>"
-	+ "<li>Hvis det må en skiløper til for å løse ut skredet og det er lite naturlig utløste skred så er vi kanksje på en 2-Moderat.</li>"
-	+ "<li>Hvis det ikke registreres faretegn, kanskje vi er på 1-Liten.</li>"
+	+ "<li>Hvis det dr&oslash;nner eller g&aring;r naturlige skred vet vi at vi trolig er p&aring; en faregrad 3-Markert.</li>"
+	+ "<li>Hvis det m&aring; en skil&oslash;per til for &aring; l&oslash;se ut skredet og det er lite naturlig utl&oslash;ste skred s&aring; er vi kanksje p&aring; en 2-Moderat.</li>"
+	+ "<li>Hvis det ikke registreres faretegn, kanskje vi er p&aring; 1-Liten.</li>"
 	+ "</ul>"
 	+ "<p>Det er noen ”kanksje’er” og ”trolig’er” i teksten over. Faretegn forteller bare en liten del av totalbildet. De er indikatorer.</p>"
-	+ "<p>Derfor er det også viktig at man legger til anslått utstrekning på faretegnet.</p>";
+	+ "<p>Derfor er det ogs&aring; viktig at man legger til ansl&aring;tt utstrekning p&aring; faretegnet.</p>";
 
-SNOW_INCIDENT =  "SNOW_INCIDENT";
+SNOW_INCIDENT =  
+	" <h3>Ulykker, nestenulykker og hendelser</h3>"
+	+ "<div>Vi &oslash;nkser &aring; l&aelig;re av deres feil. Vi &oslash;nsker at dere skal l&aelig;re av andres feil.</div>
+	+ "<ul>"
+	+ "<li>Hvis man legger inn info n&aring;r det skjer noe kan alle l&aelig;re av ulykken og den kan ha en forebyggende effekt.</li>"
+	+ "<li>Ut fra en beredskapstankegang er det nyttig &aring; f&aring; vite om hendelser for &aring; kunne oppdatere innholdet s&aring; det blir mer relevant for det som skjer.</li>"
+	+ "</ul>"
+	+ "<div>Vi kan ogs&aring; l&aelig;re av mange hendelser over tid.</div>"
+	+ "<ul>"
+	+ "<li>Hvis det registreres at skred ofte g&aring;r langt ned mot bebyggelse, kan det danne grunnlag for sikkring.&nbsp;</li>"
+	+ "<li>Hvis det registreres mange nestenulykker hvor ABS sekker har v&aelig;rt involvert, kan man f&aring; gode tall p&aring; virkningen.</li>"
+	+ "</ul>;
 
 ICE_PICTURE =  "ICE_PICTURE";
               
@@ -95,4 +110,7 @@ DIRT_DANGERSIGN = "DIRT_DANGERSIGN";
 
 DIRT_INCIDENT = "DIRT_INCIDENT";
 
+/*
+ * Om appen slik funnet bak logoen på forsiden
+ */
 ABOUT_REGOBS = "Mer text her. ABOUT_REGOBS";
