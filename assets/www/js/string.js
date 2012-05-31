b5fe34b5801e0a8340733d4a881696332d300723
ragnarekker <ragnar.ekker@gmail.com>
2012-05-31 21:56:06 +0300

    taking away infopages in main branch

--- assets/www/js/string.js
+++ assets/www/js/string.js
@@ -18,5 +18,3 @@ OK = "Ok";
 SEND_EMAIL = "Send epost";
 
 HENDELSE = "Ulykke / Hendelse";
-
-SNOW_PICTURE ="<H3>Hvordan ta et godt bilde</H3><ul><li>Tenk på at et bilde skal beskrive noe som kan nyttes av andre når de skal gjøre seg en vurdering av forholdene.</li><li>Hvis bildet skal sees i sammenheng med en annen observasjon, velg den i nedtrekksmenyen.</li><li>Svært nyttige bilder å få inn er: snøfordelig i området, skred, skavler, snøprofilet ift terrenget rundt, fete skibilder.</li><li>Ikke så nyttige bilder å få inn: noen som graver et snøprofil.</li></ul>";
