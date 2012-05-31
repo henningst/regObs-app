bb56359ceab3c36bc88aafb2fdb35d6e69ada81f
ragnarekker <ragnar.ekker@gmail.com>
2012-05-31 22:24:19 +0300

    Update assets/www/js/string.js

--- assets/www/js/string.js
+++ assets/www/js/string.js
@@ -55,46 +55,44 @@ DIRT_TEXT =
 		"Gjelder for fylket/ varslingsregionen.",
 	 ];
 
-SNOW_PICTURE =  "<H3>Hvordan ta et godt bilde</H3>"
-              + "<ul>" 
-              + "<li>Tenk på at et bilde skal beskrive noe som kan nyttes av andre når de skal gjøre seg en vurdering av forholdene.</li>"
-              + "<li>Hvis bildet skal sees i sammenheng med en annen observasjon, velg den i nedtrekksmenyen.</li>"
-              + "<li>Svært nyttige bilder å få inn er: snøfordelig i området, skred, skavler, snøprofilet ift terrenget rundt, fete skibilder.</li>"
-              + "<li>Ikke så nyttige bilder å få inn: noen som graver et snøprofil.</li>"
-              + "</ul>";
+SNOW_PICTURE =  
+	"<H3>Hvordan ta et godt bilde</H3>"
+	+ "<ul>" 
+	+ "<li>Tenk på at et bilde skal beskrive noe som kan nyttes av andre når de skal gjøre seg en vurdering av forholdene.</li>"
+	+ "<li>Hvis bildet skal sees i sammenheng med en annen observasjon, velg den i nedtrekksmenyen.</li>"
+	+ "<li>Svært nyttige bilder å få inn er: snøfordelig i området, skred, skavler, snøprofilet ift terrenget rundt, fete skibilder.</li>"
+	+ "<li>Ikke så nyttige bilder å få inn: noen som graver et snøprofil.</li>"
+	+ "</ul>";
               
-SNOW_DANGERSIGN =  "<H3>Hvordan ta et godt bilde</H3>"
-              + "<ul>" 
-              + "<li>Tenk på at et bilde skal beskrive noe som kan nyttes av andre når de skal gjøre seg en vurdering av forholdene.</li>"
-              + "<li>Hvis bildet skal sees i sammenheng med en annen observasjon, velg den i nedtrekksmenyen.</li>"
-              + "<li>Svært nyttige bilder å få inn er: snøfordelig i området, skred, skavler, snøprofilet ift terrenget rundt, fete skibilder.</li>"
-              + "<li>Ikke så nyttige bilder å få inn: noen som graver et snøprofil.</li>"
-              + "</ul>";
+SNOW_DANGERSIGN =
+	"</h3>Hva er det med disse faretegnene egentlig?</h3>"
+	+ "<p>Faretegn kan man bruke til å vurdere hvor stabil snøen er.</p>"
+	+ "<ul>"
+	+ "<li>Hvis det drønner eller går naturlige skred vet vi at vi trolig er på en faregrad 3-Markert.</li>"
+	+ "<li>Hvis det må en skiløper til for å løse ut skredet og det er lite naturlig utløste skred så er vi kanksje på en 2-Moderat.</li>"
+	+ "<li>Hvis det ikke registreres faretegn, kanskje vi er på 1-Liten.</li>"
+	+ "</ul>"
+	+ "<p>Det er noen ”kanksje’er” og ”trolig’er” i teksten over. Faretegn forteller bare en liten del av totalbildet. De er indikatorer.</p>"
+	+ "<p>Derfor er det også viktig at man legger til anslått utstrekning på faretegnet.</p>";
 
-SNOW_INCIDENT =  "<H3>Hvordan ta et godt bilde</H3>"
-              + "<ul>" 
-              + "<li>Tenk på at et bilde skal beskrive noe som kan nyttes av andre når de skal gjøre seg en vurdering av forholdene.</li>"
-              + "<li>Hvis bildet skal sees i sammenheng med en annen observasjon, velg den i nedtrekksmenyen.</li>"
-              + "<li>Svært nyttige bilder å få inn er: snøfordelig i området, skred, skavler, snøprofilet ift terrenget rundt, fete skibilder.</li>"
-              + "<li>Ikke så nyttige bilder å få inn: noen som graver et snøprofil.</li>"
-              + "</ul>";
+SNOW_INCIDENT =  "SNOW_INCIDENT";
 
-ICE_PICTURE =  "<H3>Hvordan ta et godt isbilde</H3>";
+ICE_PICTURE =  "ICE_PICTURE";
               
-ICE_DANGERSIGN = "";
+ICE_DANGERSIGN = "ICE_DANGERSIGN";
 
-ICE_INCIDENT = "";
+ICE_INCIDENT = "ICE_INCIDENT";
 
-WATER_PICTURE =  "<H3>Hvordan ta et godt vannbilde</H3>";
+WATER_PICTURE =  "WATER_PICTURE";
               
-WATER_DANGERSIGN = "";
+WATER_DANGERSIGN = "WATER_DANGERSIGN";
 
-WATER_INCIDENT = "";
+WATER_INCIDENT = "WATER_INCIDENT";
 
-DIRT_PICTURE =  "<H3>Hvordan ta et godt vannbilde</H3>";
+DIRT_PICTURE =  "DIRT_PICTURE";
               
-DIRT_DANGERSIGN = "";
+DIRT_DANGERSIGN = "DIRT_DANGERSIGN";
 
-DIRT_INCIDENT = "";
+DIRT_INCIDENT = "DIRT_INCIDENT";
 
-ABOUT_REGOBS = "Mer text her";
+ABOUT_REGOBS = "Mer text her. ABOUT_REGOBS";
