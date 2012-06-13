747a1fc729b03a0e04e33451b0b31df748b1db5d
aslak johannessen <aslak@nlink.no>
2012-06-13 17:32:20 +0200

    Refactored water and ice to use the super_page

--- assets/www/js/ice/ice.js
+++ assets/www/js/ice/ice.js
@@ -1,85 +1,26 @@
-var NUMBERS_AFTER_KOMMA = 1000000; //6 numbers precision
-
 var ice_page = {
-		
-	latitute: 0,
-	
-	longitude: 0,
-	
-	komm_nr: 0,
-	
-	omrade_id: 0,
-
-	pos_obj: null,
-		
+	danger_store : function() { return main.store.getIce(); },
 	// onSuccess Callback
 	//   This method accepts a `Position` object, which contains
 	//   the current GPS coordinates
 	//
 	onSuccess: function(position) {
-		position.taken = new Date();
-		ice_page.pos_obj = position;
-		
-		var source = new Proj4js.Proj('EPSG:4326');    //source coordinates will be in Longitude/Latitude
-		var dest = new Proj4js.Proj('EPSG:32633');     //destination coordinates for Norway
-		
-		var p = new Proj4js.Point(position.coords.longitude, position.coords.latitude);  
-		Proj4js.transform(source, dest, p);
-		
-		ice_page.longitude= Math.round(p.x);
-		ice_page.latitute  = Math.round(p.y);
-
-		jQuery('.position_header_position').html("UTM33 ( &plusmn;" +position.coords.accuracy +"m )");
-		jQuery('.position_header_town').html("N:" +Math.round(p.y) +" &Oslash;:" +Math.round(p.x));
-		
+		this.updatePagePosition(position);
+		this.displayPosition(position);
+				
 		GetObjectFromServer(new PositionDetails(ice_page.latitute, ice_page.longitude), ice_page.onKommuneResult);
 		GetObjectFromServer(new AreaInformation(ice_page.latitute, ice_page.longitude), ice_page.onAreaInformationResult);
 	},
 	
 	updateLocation : function() 
 	{
-		main.store.getIce().setLatLong(ice_page.latitute, ice_page.longitude);
+		geo.requestPosition('ice_page.setStoredLocation');
 	},
 
-	onAreaInformationResult: function(data) {
-		var res = JSON.parse(data);
-
-		if(res != null) {
-			ice_page.omrade_id = res.features[0].attributes.OMRAADEID +OMRAADE_ID_OFFSET;
-			jQuery('.county_b').html(res.features[0].attributes.OMRAADENAVN);
-		}		
-	},
-
-	// onError Callback receives a PositionError object
-	//
-	onError: function(error) {
-		jQuery('.position_header_town').html("no" +" , " +"geodata");
-
-		ice_page.longitude = 0;
-		ice_page.latitute  = 0;
-		ice_page.komm_nr = 0;
-		ice_page.omrade_id = 0;
-	},
-	
-	onKommuneResult : function(data) {
-		var res = JSON.parse(data);
-
-		if(res != null) {
-			jQuery(".county_a").html(res.features[0].attributes.KOMMNAVN);
-			ice_page.komm_nr = res.features[0].attributes.KOMM_NR;
-		}
-	},
-	
 	doMeasurement: function() {
 		geo.requestPosition('ice_page.onSuccess');
 	},
 	
-	add: function(id)
-	{
-		var i = parseInt($(id).innerHTML) +1;
-		$(id).innerHTML = i;
-	},
-   
 	afterSendRegistration: function() {
 		$('ice_picture_count').innerHTML = 0;
 		$('ice_faresign_count').innerHTML = 0;
@@ -88,25 +29,16 @@ var ice_page = {
 	init: function() {
 		$('header_middle_text').innerHTML = "Is";
 		
-		if(ice_page.pos_obj != null) {
-			if(((new Date()).getTime() - ice_page.pos_obj.taken.getTime()) / 1000 / 60 < 1) {
-				ice_page.doMeasurement();
-			}
-		} else {
-			ice_page.doMeasurement();
-		}
+		ice_page.doMeasurement();
 		
 		var iceStore = main.store.getIce();
 
-		$('ice_faresign_count').innerHTML = iceStore.getObs().length
-		$('ice_picture_count').innerHTML = iceStore.getPictures().length
+		$('ice_faresign_count').innerHTML = iceStore.getObs().length;
+		$('ice_picture_count').innerHTML = iceStore.getPictures().length;
 		
-		if(DataAccess.get(STARTUP_PAGE) != undefined && parseInt(DataAccess.get(STARTUP_PAGE)) == ICE) {
+		this.showStar();
+	}
+};
 
-			jQuery("#star").attr('src', 'img/stared.png');
-		} else {
-			jQuery("#star").attr('src', 'img/notstared.png');
-		}
-	},
-}
+jQuery.extend(ice_page, super_page);
 
--- assets/www/js/water/water.js
+++ assets/www/js/water/water.js
@@ -1,84 +1,26 @@
-var NUMBERS_AFTER_KOMMA = 1000000; //6 numbers precision
-
 var water_page = {
-		
-	latitute: 0,
-	
-	longitude: 0,
-	
-	komm_nr: 0,
-	
-	omrade_id: 0,
-
-	pos_obj: null,
-	
+	danger_store : function() { return main.store.getWater(); },
 	// onSuccess Callback
 	//   This method accepts a `Position` object, which contains
 	//   the current GPS coordinates
 	//
 	onSuccess: function(position) {
-		position.taken = new Date();
-		water_page.pos_obj = position;
-		
-		var source = new Proj4js.Proj('EPSG:4326');    //source coordinates will be in Longitude/Latitude
-		var dest = new Proj4js.Proj('EPSG:32633');     //destination coordinates for Norway
-		
-		var p = new Proj4js.Point(position.coords.longitude, position.coords.latitude);  
-		Proj4js.transform(source, dest, p);
-		
-		water_page.longitude = Math.round(p.x);
-		water_page.latitute  = Math.round(p.y);
-
-		jQuery('.position_header_position').html("UTM33 ( &plusmn;" +position.coords.accuracy +"m )");
-		jQuery('.position_header_town').html("N:" +Math.round(p.y) +" &Oslash;:" +Math.round(p.x));
-		
+		this.updatePagePosition(position);
+		this.displayPosition(position);
+				
 		GetObjectFromServer(new PositionDetails(water_page.latitute, water_page.longitude), water_page.onKommuneResult);
 		GetObjectFromServer(new AreaInformation(water_page.latitute, water_page.longitude), water_page.onAreaInformationResult);
 	},
 	
 	updateLocation : function() 
 	{
-		main.store.getWater().setLatLong(water_page.latitute, water_page.longitude);
+		geo.reqestPosition('water_page.setStoredLocation');
 	},
 
-	onAreaInformationResult: function(data) {
-		var res = JSON.parse(data);
-
-		if(res != null) {
-			water_page.omrade_id = res.features[0].attributes.OMRAADEID +OMRAADE_ID_OFFSET;
-			jQuery('.county_b').html(res.features[0].attributes.OMRAADENAVN);
-		}		
-	},
-
-	// onError Callback receives a PositionError object
-	//
-	onError: function(error) {
-		jQuery('.position_header_town').html("no" +" , " +"geodata");
-		water_page.longitude = 0;
-		water_page.latitute  = 0;
-		water_page.komm_nr = 0;
-		water_page.omrade_id = 0;
-	},
-	
-	onKommuneResult : function(data) {
-		var res = JSON.parse(data);
-
-		if(res != null) {
-			jQuery(".county_a").html(res.features[0].attributes.KOMMNAVN);
-			water_page.komm_nr = res.features[0].attributes.KOMM_NR;
-		}
-	},
-	
 	doMeasurement: function() {
 		geo.requestPosition('water_page.onSuccess');
 	},
 	
-	add: function(id)
-	{
-		var i = parseInt($(id).innerHTML) +1;
-		$(id).innerHTML = i;
-	},
-   
 	afterSendRegistration: function() {
 		$('water_faresign_count').innerHTML = 0;
 		$('water_picture_count').innerHTML = 0;
@@ -87,25 +29,16 @@ var water_page = {
 	init: function() {
 		$('header_middle_text').innerHTML = "Vann";
 		
-		if(water_page.pos_obj != null) {
-			if(((new Date()).getTime() - water_page.pos_obj.taken.getTime()) / 1000 / 60 < 1) {
-				water_page.doMeasurement();
-			}
-		} else {
-			water_page.doMeasurement();
-		}
+		water_page.doMeasurement();
 		
 		var waterStore = main.store.getWater();
 
 		$('water_faresign_count').innerHTML = waterStore.getObs().length
 		$('water_picture_count').innerHTML = waterStore.getPictures().length
 		
-		if(DataAccess.get(STARTUP_PAGE) != undefined && parseInt(DataAccess.get(STARTUP_PAGE)) == WATER) {
-
-			jQuery("#star").attr('src', 'img/stared.png');
-		} else {
-			jQuery("#star").attr('src', 'img/notstared.png');
-		}
+		this.showStar();
 	},
-}
+};
+
+jQuery.extend(water_page, super_page);
 
