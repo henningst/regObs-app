9bac33f66d3d0c31615f085f3e7f0f0a02f9e806
aslak johannessen <aslak@nlink.no>
2012-06-13 17:15:59 +0200

    Refactored the common stuff out into a super page

--- assets/www/js/snow/snow.js
+++ assets/www/js/snow/snow.js
@@ -1,5 +1,71 @@
 var NUMBERS_AFTER_KOMMA = 1000000; //6 numbers precision
 
+var super_page = {
+		updatePagePosition: function(position){
+			this.pos_obj = position;
+			
+			var source = new Proj4js.Proj('EPSG:4326');    //source coordinates will be in Longitude/Latitude
+			var dest = new Proj4js.Proj('EPSG:32633');     //destination coordinates for Norway
+			
+			var p = new Proj4js.Point(position.coords.longitude, position.coords.latitude);  
+			Proj4js.transform(source, dest, p);
+			
+			this.longitude= Math.round(p.x);
+			this.latitute  = Math.round(p.y);
+		},
+		add: function(id)
+		{
+			var i = parseInt($(id).innerHTML) +1;
+			$(id).innerHTML = i;
+		},
+		
+		setStoredLocation : function(position){
+			this.updatePagePosition(position);
+			this.danger_store().setLatLong(this.latitute, this.longitude);
+		},
+		
+		onAreaInformationResult: function(data) {
+			var res = JSON.parse(data);
+
+			if(res != null) {
+				this.omrade_id = res.features[0].attributes.OMRAADEID +OMRAADE_ID_OFFSET;
+				jQuery('.county_b').html(res.features[0].attributes.OMRAADENAVN);
+			}		
+		},
+		
+		// onError Callback receives a PositionError object
+		//
+		onError: function(error) {
+			jQuery('.position_header_town').html("no" +" , " +"geodata");
+
+			this.longitude = 0;
+			this.latitute  = 0;
+			this.komm_nr = 0;
+			this.omrade_id = 0;
+		},
+		
+		onKommuneResult : function(data) {
+			var res = JSON.parse(data);
+
+			
+			if(res != null) {
+				jQuery(".county_a").html(res.features[0].attributes.KOMMNAVN);
+				this.komm_nr = res.features[0].attributes.KOMM_NR;
+			}
+		},
+		
+		
+		showStar : function(){
+			if(DataAccess.get(STARTUP_PAGE) != undefined && parseInt(DataAccess.get(STARTUP_PAGE)) == SNOW) {
+	
+				jQuery("#star").attr('src', 'img/stared.png');
+			} else {
+	
+				jQuery("#star").attr('src', 'img/notstared.png');
+			}
+		}
+};
+
 var snow_page = {
 		
 	latitute: 0,
@@ -11,6 +77,8 @@ var snow_page = {
 	omrade_id: 0,
 	
 	pos_obj: null,
+	
+	danger_store : function(){ return main.store.getSnow(); },
 		
 	// onSuccess Callback
 	//   This method accepts a `Position` object, which contains
@@ -27,70 +95,15 @@ var snow_page = {
 		
 	},
 	
-	updatePagePosition: function(position){
-		snow_page.pos_obj = position;
-		
-		var source = new Proj4js.Proj('EPSG:4326');    //source coordinates will be in Longitude/Latitude
-		var dest = new Proj4js.Proj('EPSG:32633');     //destination coordinates for Norway
-		
-		var p = new Proj4js.Point(position.coords.longitude, position.coords.latitude);  
-		Proj4js.transform(source, dest, p);
-		
-		snow_page.longitude= Math.round(p.x);
-		snow_page.latitute  = Math.round(p.y);
-	},
-	
-	setStoredLocation : function(position){
-		snow_page.updatePagePosition(position);
-		main.store.getSnow().setLatLong(snow_page.latitute, snow_page.longitude);
-	},
-	
 	updateLocation : function() 
 	{
 		console.log("updateing position");
 		geo.requestPosition('snow_page.setStoredLocation');
 	},
 	
-	onAreaInformationResult: function(data) {
-		var res = JSON.parse(data);
-
-		if(res != null) {
-			snow_page.omrade_id = res.features[0].attributes.OMRAADEID +OMRAADE_ID_OFFSET;
-			jQuery('.county_b').html(res.features[0].attributes.OMRAADENAVN);
-		}		
-	},
-	
-	// onError Callback receives a PositionError object
-	//
-	onError: function(error) {
-		jQuery('.position_header_town').html("no" +" , " +"geodata");
-
-		snow_page.longitude = 0;
-		snow_page.latitute  = 0;
-		snow_page.komm_nr = 0;
-		snow_page.omrade_id = 0;
-	},
-	
-	onKommuneResult : function(data) {
-		var res = JSON.parse(data);
-
-		
-		if(res != null) {
-			jQuery(".county_a").html(res.features[0].attributes.KOMMNAVN);
-			snow_page.komm_nr = res.features[0].attributes.KOMM_NR;
-		}
-	},
-	
-	
 	doMeasurement: function() {
 		geo.last_page_location = 'snow_page.onSuccess';
 		geo.requestPosition('snow_page.onSuccess');
-	},
-	
-	add: function(id)
-	{
-		var i = parseInt($(id).innerHTML) +1;
-		$(id).innerHTML = i;
 	},
    
 	afterSendRegistration: function() {
@@ -105,15 +118,11 @@ var snow_page = {
 
 		var snowStore = main.store.getSnow();
 
-		$('snow_faresign_count').innerHTML = snowStore.getObs().length
-		$('snow_picture_count').innerHTML = snowStore.getPictures().length
+		$('snow_faresign_count').innerHTML = snowStore.getObs().length;
+		$('snow_picture_count').innerHTML = snowStore.getPictures().length;
 		
-		if(DataAccess.get(STARTUP_PAGE) != undefined && parseInt(DataAccess.get(STARTUP_PAGE)) == SNOW) {
+		this.showStar();
+	}
+};
 
-			jQuery("#star").attr('src', 'img/stared.png');
-		} else {
-
-			jQuery("#star").attr('src', 'img/notstared.png');
-		}
-	},
-}
+jQuery.extend(snow_page, super_page);
