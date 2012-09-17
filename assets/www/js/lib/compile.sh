8ba6198bb75d69500e700120938542d5566f5be3
aslak johannessen <aslak@nlink.no>
2012-09-17 10:40:51 +0200

    added auto compiler to handlebars

--- assets/www/js/lib/compile.sh
+++ assets/www/js/lib/compile.sh
@@ -1,1 +1,5 @@
  coffee --watch -c -b -o ../connection/ *.coffee & cd database/; coffee --watch -c -b -o ../../database/ *.coffee & 
+
+ cd ..
+ while :; do handlebars ./templates/* -f ../templates.js; sleep 2; done
+
--- assets/www/js/templates.js
+++ assets/www/js/templates.js
@@ -13,6 +13,6 @@ templates['sendGroup'] = template(function (Handlebars,depth0,helpers,partials,d
   foundHelper = helpers.hazard;
   if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
   else { stack1 = depth0.hazard; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
-  buffer += escapeExpression(stack1) + "');\">\n        <button class=\"groupButton confirmButton w_button w_radius w_bg_light takeAllWidth\" >\n            <span class=\"ellipsisText\">Gruppe</span>\n        </button>\n        <input type=\"hidden\" class=\"selectedGroup\"/>\n      </div>\n  </div>\n  \n  <div class=\"groups hidden\"> </div>";
+  buffer += escapeExpression(stack1) + "');\">\n        <button class=\"groupButton confirmButton w_button w_radius w_bg_light takeAllWidth\" >\n            <span class=\"ellipsisText\">Gruppe</span>\n        </button>\n        <input type=\"hidden\" class=\"selectedGroup\"/>\n      </div>\n  </div>\n  \n  <div class=\"groups hidden\"> </div>\n  \n  ";
   return buffer;});
 })();
